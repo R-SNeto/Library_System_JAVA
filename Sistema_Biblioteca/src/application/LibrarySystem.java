4d9d2c65fb4927c6325cfa206ff7d384c6d53627
Neto <rneto879@gmail.com>
2026-02-27 11:05:56 -0300

    Update LibrarySystem.java
    Adição de comentários para melhor legibilidade

--- Sistema_Biblioteca/src/application/LibrarySystem.java
+++ Sistema_Biblioteca/src/application/LibrarySystem.java
@@ -5,7 +5,8 @@ import menu.MainUI;
 public class LibrarySystem {
 
 	public static void main(String[] args) {
-		
+
+		//Starts the program
 		MainUI mainUi = new MainUI();
 		mainUi.startProgram();
 		
